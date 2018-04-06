3229c7254ccd1965c282b75d6bc8d42f7f9bcee3
kiboliu <kibo.liu95@gmail.com>
2018-04-06 02:09:30 -0400

    Update KNN

--- KNN-ProductTwo/KNN.py
+++ KNN-ProductTwo/KNN.py
@@ -8,22 +8,18 @@ import weka.core.jvm as jvm
 # loader = Loader(classname="weka.core.converters.ArffLoader")
 # data = loader.load_file("trainProdIntro.binary.arff")
 
-def getVector(matrix):
-    Lambda, Q = np.linalg.eig(matrix)
-    return Q
-
-
 def tranfer(info):
-    ls=[]
-    ls.extend(list(mapOne.get(info[0])))
-    ls.extend(list(mapTwo.get(info[1])))
-    ls.append(1.5*float(info[2]))
-    ls.append(10*float(info[3]))
-    ls.extend(list(mapThree.get(info[4])))
-    ls.extend(list(mapFour.get(info[5])))
-    ls.append(1.5*float(info[6]))
-    ls.append(2*float(info[7]))
-    return ls
+    lsone=[]
+    lstwo=[]
+    lsone.append(mapOne.get(info[0]))
+    lsone.append(mapTwo.get(info[1]))
+    lstwo.append(float(info[2]))
+    lstwo.append(float(info[3]))
+    lsone.append(mapThree.get(info[4]))
+    lsone.append(mapFour.get(info[5]))
+    lstwo.append(float(info[6]))
+    lstwo.append(float(info[7]))
+    return lsone, lstwo
 
 def Norm(matrix):
     minVals = matrix.min(0)
@@ -35,17 +31,22 @@ def Norm(matrix):
     norm = norm / tile(ranges, (m, 1))
     return norm
 
-def classify(input, dataset, labels, k):
-    num=dataset.shape[0]
-    # calculate euclidean distance
-    # tile(A, reps): Construct an array by repeating A reps times
-    # the following copy numSamples rows for dataSet
-    input = tranfer(input)
-    diff = tile(input, (num, 1)) - dataset  # Subtract element-wise
-    squaredDiff = diff ** 2  # squared for the subtract
-    squaredDist = sum(squaredDiff, axis=1)  # sum is performed by row
-    distance = squaredDist ** 0.5
-
+def classify(input, matrixone, matrixtwo, labels, k, minVals, maxVals):
+    inputone, inputtwo = tranfer(input)
+    ranges = maxVals - minVals
+    inputtwo = inputtwo - minVals
+    inputtwo = inputtwo / ranges
+    num = matrixtwo.shape[0]
+    diff = tile(inputtwo, (num, 1)) - matrixtwo
+    squared_diff = diff ** 2
+    squared_dist = sum(squared_diff, axis=1)
+    distance=[]
+    row=matrixone.shape[0]
+    for index in range(row):
+        cur = 1 - initialOne[matrixone[index][0]][inputone[0]] + 1 - initialTwo[matrixone[index][1]][inputone[1]] + 1 - initialThree[matrixone[index][2]][inputone[2]] + 1 - initialFour[matrixone[index][3]][inputone[3]]
+        cur += squared_dist[index]
+        cur = cur ** 0.5
+        distance.append(cur)
     # sort the distance
     sortedDistIndices = argsort(distance)
 
@@ -70,55 +71,56 @@ if __name__ == '__main__':
     initialOne = array(
         [[1, 0, 0.1, 0.3, 0.2], [0, 1, 0, 0, 0], [0.1, 0, 1, 0.2, 0.2], [0.3, 0, 0.2, 1, 0.1], [0.2, 0, 0.2, 0.1, 1]])
     wordOne = ['Loan', 'Bank_Account', 'CD', 'Mortgage', 'Fund']
-    matrixOne = getVector(initialOne)
     mapOne = {}
     for index in range(len(wordOne)):
-        mapOne[wordOne[index]] = matrixOne[index]
+        mapOne[wordOne[index]] = index
 
     initialTwo = array(
         [[1, 0.2, 0.1, 0.2, 0], [0.2, 1, 0.2, 0.1, 0], [0.1, 0.2, 1, 0.1, 0], [0.2, 0.1, 0.1, 1, 0], [0, 0, 0, 0, 1]])
     wordTwo = ['Business', 'Professional', 'Student', 'Doctor', 'Other']
-    matrixTwo = getVector(initialTwo)
     mapTwo = {}
     for index in range(len(wordTwo)):
-        mapTwo[wordTwo[index]] = matrixTwo[index]
+        mapTwo[wordTwo[index]] = index
 
     initialThree = array([[1, 0.1, 0], [0.1, 1, 0.1], [0, 0.1, 1]])
     wordThree = ['Small', 'Medium', 'Large']
-    matrixThree = getVector(initialThree)
     mapThree = {}
     for index in range(len(wordThree)):
-        mapThree[wordThree[index]] = matrixThree[index]
+        mapThree[wordThree[index]] = index
 
     initialFour = array([[1, 0.8, 0, 0], [0.8, 1, 0.1, 0.5], [0, 0.1, 1, 0.4], [0, 0.5, 0.4, 1]])
     wordFour = ['Full', 'Web&Email', 'Web', 'None']
-    matrixFour = getVector(initialFour)
     mapFour = {}
     for index in range(len(wordFour)):
-        mapFour[wordFour[index]] = matrixFour[index]
+        mapFour[wordFour[index]] = index
 
     # construct train data matrix and labels
-    matrix = []
+    matrixone = []
+    matrixtwo = []
     label = []
-    with open('train4') as finput:
+    with open('train5') as finput:
         for a in finput:
             if a[-1] == '\n':
                 a=a[:-1]
             a = a.split(",")
             if len(a) == 9:
                 label.append(a[8])
-                a = tranfer(a)
-                matrix.append(a)
-    dataset = np.asarray(matrix)
-    norm = Norm(dataset)
+                one, two = tranfer(a)
+                matrixone.append(one)
+                matrixtwo.append(two)
+    matrixone = np.asarray(matrixone)
+    matrixtwo = np.asarray(matrixtwo)
+    minVals = matrixtwo.min(0)
+    maxVals = matrixtwo.max(0)
+    matrixtwo = Norm(matrixtwo)
     count = 0
     total= 0
-    with open('test4') as ftest:
+    with open('test5') as ftest:
         for b in ftest:
             total+=1
             actual=b[-2]
             b = b[:-2].split(",")
-            res = classify(b, dataset, label, 5)
+            res = classify(b, matrixone, matrixtwo, label, 6, minVals, maxVals)
             if (actual == res):
                 count+=1
     print(count/total)
